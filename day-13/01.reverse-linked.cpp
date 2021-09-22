cc12eda4d0b2ab35bfd59b774ecfc0e5f1b11bf3
Siddhartha <sidduinevitable@gmail.com>
2021-09-22 22:10:04 +0530

    day-13: (problem 1) Reverse linked list

--- day-13/01.reverse-linked.cpp
+++ day-13/01.reverse-linked.cpp
@@ -9,3 +9,18 @@ public:
     return temp;
   }
 };
+
+// Method 2
+class Solution {
+public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* cur = NULL;
+    while (head) {
+      ListNode* temp = head->next;
+      head->next = cur;
+      cur = head;
+      head = temp;
+    }
+    return cur;
+  }
+};
