3ab1875487448646bed67a46c62753079a234c9f
Siddhartha <sidduinevitable@gmail.com>
2021-10-01 23:21:06 +0530

    day-

---
+++ day-16/02.next-greater-element-II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  vector<int> nextGreaterElements(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n);
+    stack<long> s;
+    for (int i = 2*n - 1; i >= 0; i--) {
+      while (!s.empty() && s.top() <= arr[i  % n]) {
+        s.pop();
+      }
+      if (i < n) {
+        if (s.empty()) {
+          result[i] = -1;
+        } else {
+          result[i] = s.top();
+        }
+      }
+      s.push(arr[i % n]);
+    }
+    return result;
+  }
+};
